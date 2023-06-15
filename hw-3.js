36829f78ba5d59e5c4a93ca5ff2dbb57136142cd
Даниил <chirun1999@mail.ru>
2023-06-15 19:15:02 +0700

    end hw3

---
+++ hw-3.js
@@ -0,0 +1,73 @@
+// Задание 1
+// let password = 'пароль';
+// let inputPassword = prompt ('Введите пароль');
+// if (inputPassword==password) { 
+//     alert('Пароль введен верно');
+// }
+// else {
+//      alert('Пароль введен неправильно')
+// }
+//Задание 2
+// let pelmen = 23;
+// let pelmenNew = prompt ('Введите число');
+// if (pelmenNew > 0 && pelmenNew <10) { 
+//     alert('верно');
+// }
+// else {
+//      alert('неверно')
+// }
+// Задание 3 
+// let d = 23;
+// let e = 88;
+// if (d > 100 || e > 100) { 
+//     alert('верно');
+// }
+// else {
+//      alert('неверно')
+// }
+// Задание 4
+// let a = '2';
+// let b = '3';
+// alert(Number(a)+ Number(b));
+// Задание 5
+// let monthNumber = prompt ('Введите номер месяца')
+// switch (monthNumber) {
+//     case '1':
+//         console.log ('Зима')
+//         break; 
+//     case '2':
+//         console.log ('Зима')
+//         break; 
+//     case '3':
+//         console.log ('Весна')
+//         break; 
+//     case '4':
+//         console.log ('Весна')
+//         break; 
+//     case '5':
+//         console.log ('Весна')
+//         break; 
+//     case '6':
+//         console.log ('Лето')
+//         break; 
+//     case '7':
+//         console.log ('Лето')
+//         break; 
+//     case '8':
+//         console.log ('Лето')
+//         break; 
+//     case '9':
+//         console.log ('Осень')
+//         break; 
+//     case '10':
+//         console.log ('Осень')
+//         break; 
+//     case '11':
+//         console.log ('Осень')
+//         break; 
+//     case '12':
+//         console.log ('Зима')
+//         break; 
+//     default: 
+//     console.log ('Такого месяца не существует')
+// }
