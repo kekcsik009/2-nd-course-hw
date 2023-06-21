289281d8295fa6f560a28f91af6d4314d16894cc
Даниил <chirun1999@mail.ru>
2023-06-21 18:14:46 +0700

    start hw5

---
+++ hw-5.js
@@ -0,0 +1,18 @@
+// function calc(a, b) {
+//     let sum = a + b;
+//     let mult = a * b;
+//     console.log(`Сумма параметров равна ${sum}
+//     Произведение равно ${mult}`);
+// }
+// calc (4, 5);
+// calc (10, 12);
+// function riddles(question, answer) {
+//     let userAnswer = prompt(question);
+//     if (userAnswer === answer) {
+//         console.log('угадал');
+//     }    else {
+//         console.log('не угадал');
+//     }
+// }
+// riddle('Сидит дед во сто шуб одет кто его раздевает тот слез проливает', 'лук');
+// riddle('Зимой и летом одним цветом', 'елка');
