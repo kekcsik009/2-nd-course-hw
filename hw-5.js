b55a95ed09756e51db1ecf5e7c076096b0e099a4
Даниил <chirun1999@mail.ru>
2023-06-21 21:56:09 +0700

    added 4 lesson start 5

--- hw-5.js
+++ hw-5.js
@@ -1,3 +1,4 @@
+// Теория
 // function calc(a, b) {
 //     let sum = a + b;
 //     let mult = a * b;
@@ -16,3 +17,81 @@
 // }
 // riddle('Сидит дед во сто шуб одет кто его раздевает тот слез проливает', 'лук');
 // riddle('Зимой и летом одним цветом', 'елка');
+// function sum(prepaid, salary) {
+//     return prepaid+salary;
+// }
+// function mult(day, moneyDay) {
+//     return day*moneyDay;
+// }
+
+// let month1 = sum(30, 40);
+// let month2 = sum(40, 55);
+
+// let result = month1+month2;
+// let result2 = mult (45, 3.4);
+// console.log (`Всего заработал ${result}`)
+// console.log (`Зарплата по дням ${result2}`)
+// ///////////////////////////////////////////////////////////////////////////
+// Задание 1 
+// function number(a, b) {
+//     if (a >= b) {
+//         console.log(b);
+//     }    else {
+//         console.log(a);
+//     }
+// }
+// number(4, 6);
+// number(6, 6);
+// number(6, 8);
+// Задание 2
+// function odd(a) {
+//     if (a %2== 0) {
+//         console.log(`четное число ${a}`);
+//     }    else {
+//         console.log(`нечетное число ${a}`);
+//     }    
+// }
+// odd (4);
+// odd (5);
+// Задание 3
+////3.1
+// function quadro(a) {
+//     console.log(a**2); 
+// }
+// quadro(4)
+////3.2
+// function quadro(a) {
+//     let b = a**2;
+//     console.log (b);
+//     return b;  
+// }
+// quadro(2)
+// Задание 4
+// "use strict";
+
+// let age = prompt("Сколько Вам лет?");
+
+// let printMessage;
+
+// if (age == 0 || age <=12 && age >0) {
+
+//     printMessage = function () {
+//         console.log("Привет, друг!");
+//     }
+
+// } else if (age >= 13) {
+
+//     printMessage = function () {
+//         console.log("Добро пожаловать");
+//     }
+// } else if (age <0) {
+
+//     printMessage = function () {
+//         console.log("Вы ввели неправильное значение");
+//     }
+// } 
+// printMessage();
+// Задание 5
+function name(params) {
+    
+}
