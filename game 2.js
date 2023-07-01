fab9882a23f65f95fd0538e4e38064ba9225c07e
Даниил <chirun1999@mail.ru>
2023-07-01 23:15:23 +0700

    added game end hw

--- game 2.js
+++ game 2.js
@@ -1,22 +1,7 @@
-// Первая игра
-function season(monthNumber) {
-    if (!isNaN(monthNumber) && monthNumber >=0 && monthNumber <=12)  {
-        if (monthNumber >= 1 && monthNumber <=2 || monthNumber === 12) {
-            return  console.log('Winter');
-        }  else if (monthNumber>= 3 && monthNumber <= 5){
-            return console.log('Spring');
-        }  else if (monthNumber>= 6 && monthNumber <= 8){
-            return console.log('Summer');
-        }  else  {
-            return  console.log('Аutumn');
-        } 
-    } else {
-        return console.log('Такого месяца не существует');
-    }
-}
-
-// Вторая игра
+const playButton = document.getElementById('playButton');
 const array = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+
+playButton.addEventListener("click", playGame);
 
 function words(array) {
     return array.sort(()=> Math.random() - 0.5);
